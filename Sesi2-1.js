d4f00c91ff614c5041ab762076de27f2c7cd6550
iYuuba21 <114842096+iYuuba21@users.noreply.github.com>
2023-10-05 23:11:27 +0700

    Add files via upload
    Pemograman berbasis platform

---
+++ Sesi2-1.js
@@ -0,0 +1,13 @@
+const profile = {
+    fisrtName : "Bayu",
+    lastName : "Putra",
+    age : 21,
+    isMarried : false
+};
+
+const firstName = profile.fisrtName
+const lastName = profile.lastName
+const age = profile.age
+const isMarried = profile.isMarried
+
+console.log (`Hallo Nama Saya ${firstName} ${lastName} Umur Saya ${age} menikah ${isMarried}`)
