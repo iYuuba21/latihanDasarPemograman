c5f6e3a2739d8f36a4d9630e50cc9074cb2258f6
iYuuba21 <114842096+iYuuba21@users.noreply.github.com>
2023-10-16 22:42:31 +0700

    Add files via upload

---
+++ sesi3.js
@@ -0,0 +1,22 @@
+const restaurant = {
+    name: "Tanjakan Kuring",
+    city: "Sukabumi",
+    favoriteDrink: "Jus Jeruk",
+    favoriteFood: "Tempe Goreng",
+    isVegan: true 
+  }; 
+  console.log(restaurant)
+
+  const restaurants2 = [
+    {
+      name: "Tanjakan Kuring",
+      city: "Sukabumi",
+      favoriteDrink: "Jus Jeruk",
+      favoriteFood: "Ayam Bakar",
+      isVegan: true 
+    }
+  ];
+  
+  console.table(restaurants2); 
+  
+  
